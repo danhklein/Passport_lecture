c3447f78e51599b3bccf5a13277459e1f069a84e
Daniel Klein <danhirschklein@gmail.com>
2016-03-09 10:31:46 -0700

    Created routes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,4 +5,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login Page' });
+});
+
+router.post('/login', function(req, res, next) {
+   var email = req.body.email;
+   var password = req.body.password;
+   res.send({
+    email: email,
+    password: password
+   })
+})
+
+router.get('/logout', function(req, res, next) {
+    res.redirect('/');
+})
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Registration Page' });
+});
+
+router.post('/register', function(req, res, next) {
+   var email = req.body.email;
+   var password = req.body.password;
+   res.send({
+    email: email,
+    password: password
+   })
+})
+
+
 module.exports = router;
